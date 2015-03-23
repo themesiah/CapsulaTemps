40be835c84f7de87975b46b5f6f8a8958d0f29c7
MuuSStY <jamesphelandeharo@gmail.com>
2015-03-23 16:47:20 +0100

    Base del projecte

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capsulaTempsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the capsulaTempsApp
+ */
+angular.module('capsulaTempsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capsulaTempsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the capsulaTempsApp
+ */
+angular.module('capsulaTempsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
