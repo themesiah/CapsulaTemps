df003056daa88af5093872700511f1907e38c451
vidal14 <vijom14@gmail.com>
2015-03-28 02:28:52 +0100

    Botones facebok y Google

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,55 +7,55 @@
  * # MainCtrl
  * Controller of the capsulaTempsApp
  */
- var app=angular.module('capsulaTempsApp');
+var app = angular.module('capsulaTempsApp');
 
 
 app.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
-  
-  app.controller('MainCtrl', function ($scope, $rootScope, ngDialog) {
-                            $rootScope.jsonData = '{"foo": "bar"}';
-                            $rootScope.theme = 'ngdialog-theme-default';
-                            $scope.directivePreCloseCallback = function (value) {
-                                if (confirm('Close it? MainCtrl.Directive. (Value = ' + value + ')')) {
-                                    return true;
-                                }
-                                return false;
-                            };
-                            $scope.preCloseCallbackOnScope = function (value) {
-                                if (confirm('Close it? MainCtrl.OnScope (Value = ' + value + ')')) {
-                                    return true;
-                                }
-                                return false;
-                            };
-                            $scope.open = function () {
-                                ngDialog.open({
-                                    template: '../../views/pop.html',
-                                    controller: 'InsideCtrl',
-                                    data: {foo: 'some data'}});
-                            };
-                        });
-                        app.controller('InsideCtrl', function ($scope, ngDialog) {
-                            $scope.dialogModel = {
-                                message: 'message from passed scope'
-                            };
-                            $scope.openSecond = function () {
-                                ngDialog.open({
-                                    template: '<h3><a href="" ng-click="closeSecond()">Close all by click here!</a></h3>',
-                                    plain: true,
-                                    closeByEscape: false,
-                                    controller: 'SecondModalCtrl'
-                                });
-                            };
-                        });
-                        app.controller('SecondModalCtrl', function ($scope, ngDialog) {
-                            $scope.closeSecond = function () {
-                                ngDialog.close();
-                            };
-                        });
+});
+
+app.controller('MainCtrl', function ($scope, $rootScope, ngDialog) {
+    $rootScope.jsonData = '{"foo": "bar"}';
+    $rootScope.theme = 'ngdialog-theme-default';
+    $scope.directivePreCloseCallback = function (value) {
+        if (confirm('Close it? MainCtrl.Directive. (Value = ' + value + ')')) {
+            return true;
+        }
+        return false;
+    };
+    $scope.preCloseCallbackOnScope = function (value) {
+        if (confirm('Close it? MainCtrl.OnScope (Value = ' + value + ')')) {
+            return true;
+        }
+        return false;
+    };
+    $scope.open = function () {
+        ngDialog.open({
+            template: '../../views/pop.html',
+            controller: 'InsideCtrl',
+            data: {foo: 'some data'}});
+    };
+});
+app.controller('InsideCtrl', function ($scope, ngDialog) {
+    $scope.dialogModel = {
+        message: 'message from passed scope'
+    };
+    $scope.openSecond = function () {
+        ngDialog.open({
+            template: '<h3><a href="" ng-click="closeSecond()">Close all by click here!</a></h3>',
+            plain: true,
+            closeByEscape: false,
+            controller: 'SecondModalCtrl'
+        });
+    };
+});
+app.controller('SecondModalCtrl', function ($scope, ngDialog) {
+    $scope.closeSecond = function () {
+        ngDialog.close();
+    };
+});
  
